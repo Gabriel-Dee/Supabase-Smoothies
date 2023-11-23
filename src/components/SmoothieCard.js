25b87a24d708e3f2ff642094ed9c9f17bec43a73
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2023-11-23 10:01:01 +0300

    fetching single records

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
+
 const SmoothieCard = ({ smoothie }) => {
-    return (
-      <div className="smoothie-card">
-        <h3>{smoothie.title}</h3>
-        <p>{smoothie.method}</p>
-        <div className="rating">{smoothie.rating}</div>
+  return (
+    <div className="smoothie-card">
+      <h3>{smoothie.title}</h3>
+      <p>{smoothie.method}</p>
+      <div className="rating">{smoothie.rating}</div>
+      <div className="buttons">
+        <Link to={"/" + smoothie.id}>
+          <i className="material-icons">edit</i>
+        </Link>
       </div>
-    )
-  }
-  
-  export default SmoothieCard
+    </div>
+  )
+}
+
+export default SmoothieCard
