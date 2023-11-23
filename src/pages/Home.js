ed6cc7b994e89a4f4456dfede81e327d74afcc74
Gabriel D Minzemalulu <gabriel.CSCU.2018@gmail.com>
2023-11-23 15:01:19 +0300

    ordering records in supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,18 +6,20 @@ import SmoothieCard from "../components/SmoothieCard";
 const Home = () => {
   const [fetchError, setFetchError] = useState(null);
   const [smoothies, setSmoothies] = useState(null);
+  const [orderBy, setOrderBy] = useState("created_at");
 
   const handleDelete = (id) => {
-    setSmoothies(prevSmoothies => {
-      return prevSmoothies.filter(sm => sm.id !== id)
-    })
-  }
+    setSmoothies((prevSmoothies) => {
+      return prevSmoothies.filter((sm) => sm.id !== id);
+    });
+  };
 
   useEffect(() => {
     const fetchSmoothies = async () => {
       const { data, error } = await supabase
         .from("smoothies") // from the smoothies table
-        .select(); // give us everything , but if one wants certain thong you list them in the select()
+        .select() // give us everything , but if one wants certain thong you list them in the select()
+        .order(orderBy, {ascending: false})
 
       if (error) {
         setFetchError("Could not fetch the smoothies");
@@ -29,9 +31,9 @@ const Home = () => {
         setFetchError(null);
       }
     };
-
+ 
     fetchSmoothies();
-  }, []);
+  }, [orderBy]); // the code reruns the use effect hook to apply the ordering changes since the order by is defined within the use state
 
   return (
     <div className="page home">
@@ -39,10 +41,22 @@ const Home = () => {
       {smoothies && (
         <div className="smoothies">
           {/* order-by buttons */}
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy("created_at")}>
+              Time Created
+            </button>
+            <button onClick={() => setOrderBy("title")}>Title</button>
+            <button onClick={() => setOrderBy("rating")}>Rating</button>
+          </div>
           <div className="smoothie-grid">
             {smoothies.map((smoothie) => (
               // <p>{smoothie.title}</p>
-              <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete} />
+              <SmoothieCard
+                key={smoothie.id}
+                smoothie={smoothie}
+                onDelete={handleDelete}
+              />
             ))}
           </div>
         </div>
